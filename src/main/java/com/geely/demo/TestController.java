8124b44b984cbe5b80f64c5386309297fa36ea6a
duanrui <Rui.Duan1@geely.com>
2018-07-17 15:15:52 +0800

    添加测试代码

---
+++ src/main/java/com/geely/demo/TestController.java
@@ -0,0 +1,16 @@
+package com.geely.demo;
+
+/**
+ * @ProjectName: demo
+ * @Package: com.geely.demo
+ * @Description: java类作用描述
+ * @Author: duanrui
+ * @CreateDate: 2018-07-17 15:09
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2018
+ */
+
+public class TestController {
+
+}
